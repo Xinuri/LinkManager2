13d17da72aa2a563417e8dd51b6185b6f5a21d4b
Xinuri <sergio24.kul@gmail.com>
2024-12-25 18:32:22 +0300

    Коммит 1

---
+++ LinkManager2/LinkManager2/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace LinkManager2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.listBoxLinks = new System.Windows.Forms.ListBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtLink = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnAdd.Location = new System.Drawing.Point(275, 89);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(97, 46);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Добавить";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click_1);
+            // 
+            // listBoxLinks
+            // 
+            this.listBoxLinks.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.listBoxLinks.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.listBoxLinks.FormattingEnabled = true;
+            this.listBoxLinks.ItemHeight = 16;
+            this.listBoxLinks.Location = new System.Drawing.Point(13, 56);
+            this.listBoxLinks.Name = "listBoxLinks";
+            this.listBoxLinks.Size = new System.Drawing.Size(256, 180);
+            this.listBoxLinks.TabIndex = 2;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnOpen.Location = new System.Drawing.Point(275, 141);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(97, 45);
+            this.btnOpen.TabIndex = 3;
+            this.btnOpen.Text = "Открыть";
+            this.btnOpen.UseVisualStyleBackColor = false;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click_1);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnDelete.Location = new System.Drawing.Point(275, 192);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(97, 44);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Удалить";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click_1);
+            // 
+            // txtLink
+            // 
+            this.txtLink.Location = new System.Drawing.Point(13, 13);
+            this.txtLink.Name = "txtLink";
+            this.txtLink.Size = new System.Drawing.Size(359, 20);
+            this.txtLink.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.Controls.Add(this.txtLink);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.listBoxLinks);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox listBoxLinks;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtLink;
+    }
+}
+
---
+++ LinkManager2/LinkManager2/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using System;
+
+namespace LinkManager2
+
+{
+    public partial class Form1 : Form
+
+    {
+        private List<string> links = new List<string>();
+        private const string filePath = "links.txt"; 
+
+        public Form1()
+
+        {
+
+            InitializeComponent();
+            LoadLinks(); 
+
+        }
+
+
+        private void LoadLinks()
+
+        {
+            if (File.Exists(filePath)) 
+
+            {
+
+                links = new List<string>(File.ReadAllLines(filePath)); 
+                listBoxLinks.Items.AddRange(links.ToArray()); 
+
+            }
+        }
+
+
+        private void SaveLinks()
+
+        {
+
+            File.WriteAllLines(filePath, links); 
+
+        }
+
+
+        private void btnAdd_Click_1(object sender, EventArgs e)
+
+        {
+
+            string link = txtLink.Text;
+
+            if (!string.IsNullOrWhiteSpace(link))
+
+            {
+
+                links.Add(link); 
+                listBoxLinks.Items.Add(link); 
+                txtLink.Clear(); 
+                MessageBox.Show("Ссылка добавлена."); 
+                SaveLinks(); 
+
+            }
+
+            else
+
+            {
+
+                MessageBox.Show("Введите правильную ссылку."); 
+
+            }
+
+        }
+
+
+        private void btnOpen_Click_1(object sender, EventArgs e)
+
+        {
+            if (listBoxLinks.SelectedItem != null) 
+
+            {
+                string selectedLink = listBoxLinks.SelectedItem.ToString(); 
+
+                Process.Start(new ProcessStartInfo 
+
+                {
+
+                    FileName = selectedLink,
+                    UseShellExecute = true
+
+                });
+
+
+                MessageBox.Show("Переход по ссылке...");
+
+            }
+
+            else
+
+            {
+
+                MessageBox.Show("Выберите ссылку для открытия."); 
+
+            }
+
+        }
+
+
+        private void btnDelete_Click_1(object sender, EventArgs e)
+
+        {
+            if (listBoxLinks.SelectedItem != null) 
+
+            {
+
+                string selectedLink = listBoxLinks.SelectedItem.ToString();
+                links.Remove(selectedLink); 
+                listBoxLinks.Items.Remove(selectedLink); 
+                MessageBox.Show("Ссылка удалена."); 
+                SaveLinks(); 
+
+            }
+
+            else
+
+            {
+
+                MessageBox.Show("Выберите ссылку для удаления."); 
+
+            }
+
+        }
+
+    }
+
+}
+
+
+
+
+
+
